da831af41253b3ef28b7e208cb97aa59b7bea1b5
leewyatt <leewyatt7788@gmail.com>
2024-01-23 22:03:40 +0900

    update.

--- client/src/main/java/org/example/MainApp.java
+++ client/src/main/java/org/example/MainApp.java
@@ -14,7 +14,12 @@ public class MainApp extends Application {
         Scene scene = new Scene(helloBox, 380, 200);
 
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Hello Conveyor");
+
+        // A property 'app.system-properties.stage.title = "Hello Conveyor"' is configured in conveyor.conf;
+        // After packaging the application with Conveyor, a system property named 'stage.title' is obtained;
+        // If MainApp is run directly, the default title "Tip: Launch from IDE" will be used.
+        String title = System.getProperty("stage.title", "Tip: Launch from IDE");
+        primaryStage.setTitle(title);
         primaryStage.show();
     }
 
